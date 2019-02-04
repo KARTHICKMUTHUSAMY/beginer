99878f7ac735c5c2418e5cf0d006036e835e136a
KARTHICKMUTHUSAMY <47235922+KARTHICKMUTHUSAMY@users.noreply.github.com>
2019-02-04 14:36:57 +0530

    Update check whether a number is prime or not.py

--- check whether a number is prime or not.py
+++ check whether a number is prime or not.py
@@ -1,11 +1,11 @@
 l=int(input())
 m=0
 n=int(l/2)
-for i in range(2,n)
-    if l%i==0:
-          m=1
-          break;
+for i in range(2,n):
+	if l%i==0:
+		m=1
+		break;
 if m==1:
-    print("yes")
+	print("no")
 else:
-    print("no")
+	print("yes")
