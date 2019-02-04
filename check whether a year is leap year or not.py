daf160e90bfb967ae4512376e3019780664c7ade
KARTHICKMUTHUSAMY <47235922+KARTHICKMUTHUSAMY@users.noreply.github.com>
2019-02-04 10:39:39 +0530

    Update check whether a year is leap year or not.py

--- check whether a year is leap year or not.py
+++ check whether a year is leap year or not.py
@@ -2,10 +2,10 @@ year=int(input())
 if (year % 4) == 0:
    if (year % 100) == 0:
        if (year % 400) == 0:
-           print("Yes".format(year))
+           print("yes".format(year))
        else:
-           print("No".format(year))
+           print("no".format(year))
    else:
-       print("Yes".format(year))
+       print("yes".format(year))
 else:
-   print("No".format(year))
+   print("no".format(year))
