64bf8e489bd61b3d22d6dd2c0e08cf42f1fcd399
KARTHICKMUTHUSAMY <47235922+KARTHICKMUTHUSAMY@users.noreply.github.com>
2019-02-04 09:48:49 +0530

    Update check whether a character is vowel or consonant.py

--- check whether a character is vowel or consonant.py
+++ check whether a character is vowel or consonant.py
@@ -1,5 +1,4 @@
 a=input()
-print (a)
 if(a=='A' or a=='a' or a=='E' or a=='e' or a=='I' or a=='i' or a=='O' or a=='o' or a=='U' or a=='u'):
 	print("Vowel")
 elif((a>='a' and a<='z') or (a>='A' and a<='Z')):
