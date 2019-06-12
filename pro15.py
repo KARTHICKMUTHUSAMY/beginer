c0da770e47cd8c7b9ad6f660195a883423ffe054
KARTHICKMUTHUSAMY <47235922+KARTHICKMUTHUSAMY@users.noreply.github.com>
2019-06-12 19:24:49 +0530

    Create pro15.py

---
+++ pro15.py
@@ -0,0 +1,33 @@
+
+
+a=int(input())
+
+r=list(map(int,input().split()))
+
+f=g=[]
+
+for i in range(0,a):
+
+    f=list(bin(r[i]))
+
+    f=f[2:]
+
+    g.append(f)
+
+g=sorted(g)
+
+g=g[::-1]
+
+for i in range(0,a):
+
+    b=1
+
+    z=0
+
+    for j in range(len(g[i])-1,-1,-1):
+
+        z=z+(int(g[i][j])*b)
+
+        b=b*2
+
+    print(z)
