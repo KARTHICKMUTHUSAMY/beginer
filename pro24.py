d8e35daf09de6a682ac9d7b6d51f574dbb478405
KARTHICKMUTHUSAMY <47235922+KARTHICKMUTHUSAMY@users.noreply.github.com>
2019-06-12 20:56:56 +0530

    Create pro24.py

---
+++ pro24.py
@@ -0,0 +1,25 @@
+n=int(input())
+
+v=[]
+
+k=bin(2**n-1)[2::]
+
+l=len(k)
+
+for i in range(0,2**n):
+
+	s=bin(i)[2::]
+
+	if len(s)<l:
+
+		v.append([s.count("1"),(l-len(s))*"0"+s])
+
+	else:
+
+		v.append([s.count("1"),s])
+
+v.sort()
+
+for i in range(0,len(v)):
+
+	print(v[i][1])
