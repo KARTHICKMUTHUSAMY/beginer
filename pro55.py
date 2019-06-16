5f7d0daa119c7a1aef9fc3f13ae45667b62fae7d
KARTHICKMUTHUSAMY <47235922+KARTHICKMUTHUSAMY@users.noreply.github.com>
2019-06-16 22:15:23 +0530

    Update pro55.py

--- pro55.py
+++ pro55.py
@@ -1,21 +1,21 @@
 n,k = map(int,input().split())
-a = list(map(int,input().split()))
+v = list(map(int,input().split()))
 b,c = 0,[]
-for i in range(0,len(a)):
+for i in range(0,len(v)):
   t = i
-  for p in range(0,len(a)):
+  for p in range(0,len(v)):
     for l in range(0,k):
       if l == k-1:
         try:
-          b += a[p+i]
+          b += v[p+i]
         except IndexError:
             t = t-1
-            b +=a[t]
+            b +=v[t]
       else:
-        b += a[i]
+        b += v[i]
     c.append(b)
     b = 0
-for i in range(0,len(a),k):
-  b = sum(a[i:i+k])
+for i in range(0,len(v),k):
+  b = sum(v[i:i+k])
   c.append(b)
 print(*sorted(set(c)))
